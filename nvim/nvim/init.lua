1870b0a4755e6b4888d0d60130369e9e6f6f4701
SaviolaX <withcraft1717@gmail.com>
2026-03-22 11:06:25 +0200

    Auto update from script

--- nvim/nvim/init.lua
+++ nvim/nvim/init.lua
@@ -35,6 +35,7 @@ if not (vim.uv or vim.loop).fs_stat(lazypath) then
     end
 end
 
+
 vim.opt.rtp:prepend(lazypath)
 
 -- Setup lazy.nvim
---
+++ nvim/nvim/lua/plugins/toggleterm.lua
@@ -0,0 +1,35 @@
+return {
+    {
+        "akinsho/toggleterm.nvim",
+        version = "*",
+        opts = {
+            size = function()
+                return math.floor(vim.o.lines * 0.8)
+            end,
+            open_mapping = [[<C-`>]],
+            hide_numbers = true,
+            shade_terminals = false,
+            start_in_insert = true,
+            insert_mappings = true,
+            terminal_mappings = true,
+            persist_size = true,
+            direction = "float",
+
+            float_opts = {
+                border = "rounded",
+                width = function()
+                    return math.floor(vim.o.columns * 0.8)
+                end,
+                height = function()
+                    return math.floor(vim.o.lines * 0.8)
+                end,
+            },
+        },
+        config = function(_, opts)
+            require("toggleterm").setup(opts)
+
+            -- Better terminal escape
+            vim.keymap.set("t", "<Esc>", [[<C-\><C-n>]], { desc = "Exit terminal mode" })
+        end,
+    },
+}
